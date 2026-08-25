db1c7567bea15815b50cd799e07d729b7adbc077
dfoote1021 <dfoote1021@gmail.com>
2026-08-25 17:14:13 -0400

    Create transaction-overrides.js

---
+++ assets/transaction-overrides.js
@@ -0,0 +1,20 @@
+(function () {
+  "use strict";
+
+  window.TransactionOverrides = {
+    2022: {
+      "Gavin": 26,
+      "James": 54,
+      "Rob": 45,
+      "Spencer": 39,
+      "Ken": 37,
+      "Tsizz": 46,
+      "Wes": 16,
+      "Geoff": 16,
+      "Connor": 18,
+      "Spear": 44,
+      "Dan": 88,
+      "Tom": 65
+    }
+  };
+})();
